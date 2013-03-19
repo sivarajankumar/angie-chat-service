0e0deb780d44f25cd4cd7de32c978ffe785df5d4
bodvar.sveinsson <bodvar.sveinsson@53820b2f-08ba-0d26-7542-6a7f2dfb99d4>
2013-03-19 00:07:08 +0000

    Með kick

--- trunk/angie/public/controller/ChannelsController.js
+++ trunk/angie/public/controller/ChannelsController.js
@@ -1,18 +1,55 @@
 Angie.controller('StreamMessagesController',
   function ($scope, $location, $routeParams, ChannelModel) {
     $scope.channelName = $routeParams.channelName;
+    $scope.loggedInUserId = $routeParams.userId;
+    $scope.channelName = $routeParams.channelName;
     $scope.msg = {};
 
     var handleCallback = function (msg) {
+// <<<<<<< .mine
+//         $scope.$apply(function () {
+//         $scope.msg = JSON.parse(msg.data);
+
+//         // TODO: Tékka hvort loggedInUserId sé í msg.members listanum.   Ef ekki þá skal kicka 
+//       });
+// =======
         var message = JSON.parse(msg.data);
         if( $scope.channelName === message.channelname) {
           $scope.$apply(function () {
             $scope.msg = message;
           });
         }
+//>>>>>>> .r19
     }
 
+//<<<<<<< .mine
+    $scope.isActiveMemberinChannel = function() {
+      if ($scope.msg.members !== undefined){
+        if ($scope.msg.members.length > 0) {
+          var kickmember = true;
+          for(i=0;i<$scope.msg.members.length;i=i+1){
+            if ($scope.msg.members[i] === $scope.loggedInUserId){
+             kickmember = false;
+            }
+          }
+          if (kickmember){
+            $location.path('/angiemain/' + $scope.loggedInUserId);
+            //$scope.kickMember($scope.loggedInUserId);  // has already been kicked
+            return false;
+          }
+        }
+      }
+      return true;  
+    }
+
+    $scope.kickMember = function(memberid) {
+      ChannelModel.removeChannelMember($scope.channelName, memberid);
+    }
+
+//    var source = new EventSource('/channels/' + channelName + '/messages/stream');
+//=======
     var source = new EventSource('/channels/' + $scope.channelName + '/messages/stream');
+//>>>>>>> .r19
     source.addEventListener('message', handleCallback, false);
   }
 );
@@ -25,7 +62,6 @@ Angie.controller('ListChannelController',
     $scope.selectedChannelName = $routeParams.channelName;
     $scope.loggedInUserId = $routeParams.userId;
 
-
     $scope.joinChannel = function(channelName) {
       ChannelModel.addChannelMember(channelName, userId);
       $location.path('/angiechannels/' + channelName + '/chat/' + $scope.loggedInUserId);
@@ -37,15 +73,26 @@ Angie.controller('ListChannelController',
 Angie.controller('ChatChannelController',
   function ($scope, $location, $routeParams, ChannelModel) {
     var userId = $routeParams.userId;   // BUGBUG!!! 
+/*
     var channels = ChannelModel.getChannels();  // Not used
     var messages = ChannelModel.getMessages($routeParams.channelName);
-    var currentChannel = ChannelModel.getChannelByName($routeParams.channelName);  // til að sýna members logged in, TODO: það vantar betra function til þess, getChannelMembers(channelName), sem skilar objectum en ekki strengjum.
+    $scope.currentChannel = {};
+    $scope.channelOwner = {};
+    var handleCallback_getChannelByName = function (responsedata) {
+        $scope.$apply(function () {
+        $scope.currentChannel = responsedata; //JSON.parse(msg.data)
+        $scope.channelOwner = $scope.currentChannel.data.owner;
+      });
+    }
+    var ok = ChannelModel.getChannelByNameWithCallback($routeParams.channelName,handleCallback_getChannelByName);  // til að sýna members logged in, TODO: það vantar betra function til þess, getChannelMembers(channelName), sem skilar objectum en ekki strengjum.
     $scope.channels = channels; 
     $scope.messages = messages;
-    $scope.currentChannel = currentChannel;
+    //$scope.channelOwner = currentChannel.data.owner;
+    //$scope.currentChannel   = currentChannel;
+    */
     $scope.loggedInUserId = $routeParams.userId;
     $scope.channelName = $routeParams.channelName;
-  
+  /*
     $scope.return_getChannelByName_callback = function(responsedata) {  // það er kallað í þetta frá SendChatMessageDelivered_callback, þegar getMessage HTTP GET kallið hefur skilað success.
       $scope.currentChannel = responsedata; //.data;  // Hlaða nýjustu messages inn og birta þau
       // $location.path('/angiechannels/' + $scope.channelName + '/chat/' + $scope.loggedInUserId);  // Ekki refresha síðuna fyrir þetta. Það er gert í getMessages...
@@ -55,23 +102,29 @@ Angie.controller('ChatChannelController',
       $scope.chatmsg="";  // Hreinsa innsláttarsvæðið fyrir næstu skilaboð
       $location.path('/angiechannels/' + $scope.channelName + '/chat/' + $scope.loggedInUserId);
     }  
-
+*/
     $scope.return_SendChatMessageDelivered_callback = function() {  // það er kallað í þetta frá doChat, (sendChatMessage) fallinu, þegar HTTP POST fallið hefur skilað success.
-      $scope.dummymessages_notused = ChannelModel.getMessagesWithCallback($routeParams.channelName, $scope.return_getMessagesDelivered_callback);
+      //$scope.dummymessages_notused = ChannelModel.getMessagesWithCallback($routeParams.channelName, $scope.return_getMessagesDelivered_callback);
+       $scope.chatmsg="";  // Hreinsa innsláttarsvæðið fyrir næstu skilaboð
+       $location.path('/angiechannels/' + $scope.channelName + '/chat/' + $scope.loggedInUserId);
     }  
 
     $scope.doChat = function() {
-      $scope.dummymembers_notused  = ChannelModel.getChannelByNameWithCallback($routeParams.channelName,$scope.return_getChannelByName_callback);  // refresha members
+     // $scope.dummymembers_notused  = ChannelModel.getChannelByNameWithCallback($routeParams.channelName,$scope.return_getChannelByName_callback);  // refresha members
       if ($scope.chatmsg.length == 0){ // Þegar ýtt er á enter hnappinn á ekki að senda message heldur bara að refresha skjáinn.
         // Hér er verið að refresha gögnin á skjánum:   members og messages.
-        $scope.dummymembers_notused  = ChannelModel.getChannelByNameWithCallback($routeParams.channelName,$scope.return_getChannelByName_callback); 
-        $scope.dummymessages_notused = ChannelModel.getMessagesWithCallback($routeParams.channelName, $scope.return_getMessagesDelivered_callback);
+       // $scope.dummymembers_notused  = ChannelModel.getChannelByNameWithCallback($routeParams.channelName,$scope.return_getChannelByName_callback); 
+       // $scope.dummymessages_notused = ChannelModel.getMessagesWithCallback($routeParams.channelName, $scope.return_getMessagesDelivered_callback);
       }else{
         ChannelModel.sendChatMessage($scope.channelName, $scope.loggedInUserId, $scope.chatmsg, $scope.return_SendChatMessageDelivered_callback);
       }
       //$location.path('/angiechannels/' + $scope.channelName + '/chat/' + $scope.loggedInUserId);
     }
 
+    $scope.kickMember = function(memberid) {
+      ChannelModel.removeChannelMember($scope.channelName, memberid);
+      // $location.path('/angiemain/' + $scope.loggedInUserId);
+    }
 
     $scope.leaveChannel = function() {
       ChannelModel.removeChannelMember($scope.channelName, $scope.loggedInUserId);
